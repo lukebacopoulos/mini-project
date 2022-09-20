c0287bdf4b73905f8369a7f6bc14edef7806a1e2
Yichen Wang <wyichen@bu.edu>
2022-09-19 23:37:42 -0400

    google login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { useState } from 'react'
 import './App.css';   // this is the file used for changing the ht
 import TwitterHandles from './TwitterHandles'
-
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 import handles from './handles'
+
+
+const clientId = '454678718267 - gssejg95tm7qe49kgsfoib8g0uoj0tnn.apps.googleusercontent.com';
+useEffect(() => {
+    const initClient = () => {
+        gapi.client.init({
+            clientId: clientId,
+            scope: ''
+        });
+    };
+    gapi.load('client:auth2', initClient);
+});
+const onSuccess = (res) => {
+    console.log('success:', res);
+};
+const onFailure = (err) => {
+    console.log('failed:', err);
+};
+return (
+    <GoogleLogin
+        clientId={clientId}
+        buttonText="Sign in with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+    />
+);
 
 function App() {
   const [handles, addhandle] = useState([])
@@ -14,6 +43,12 @@ function App() {
 
           <div>
               <h1>Bot or Real</h1>
+          </div>
+          <div>
+              <h1>Main Topic</h1>
+          </div>
+          <div>
+              <h1>Sentiment</h1>
           </div>
 
     </>     //fragment allows multiple arguments in return
