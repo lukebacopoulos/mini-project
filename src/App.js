cde99255e1ac9ab0db82a18732f7715112a6f647
Yichen Wang <wyichen@bu.edu>
2022-09-19 23:57:00 -0400

    google login with input twitter handle

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
-import React, { useState } from 'react'
 import './App.css';   // this is the file used for changing the ht
 import TwitterHandles from './TwitterHandles'
-import { GoogleLogin } from 'react-google-login';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { gapi } from 'gapi-script';
 import handles from './handles'
 
 
-const clientId = '454678718267 - gssejg95tm7qe49kgsfoib8g0uoj0tnn.apps.googleusercontent.com';
-useEffect(() => {
-    const initClient = () => {
-        gapi.client.init({
-            clientId: clientId,
-            scope: ''
-        });
-    };
-    gapi.load('client:auth2', initClient);
-});
-const onSuccess = (res) => {
-    console.log('success:', res);
-};
-const onFailure = (err) => {
-    console.log('failed:', err);
-};
-return (
-    <GoogleLogin
-        clientId={clientId}
-        buttonText="Sign in with Google"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-    />
-);
 
 function App() {
-  const [handles, addhandle] = useState([])
-  return (
-    <>
-    <TwitterHandles handles={handles} />
-    <input type="text" />
-    <button> Search Handle</button>
+    const [profile, setProfile] = useState([]);
+    const clientId = '386932037035-k8v833noqjk7m4auae0t83vnkrqvvg3t.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
 
-          <div>
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+    };
+
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        setProfile(null);
+    };
+
+    return (
+         <>
+        <div>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            {profile ? (
+                <div>
+                    <img src={profile.imageUrl} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
+        </div>
+        <div>
               <h1>Bot or Real</h1>
           </div>
           <div>
@@ -50,10 +68,8 @@ function App() {
           <div>
               <h1>Sentiment</h1>
           </div>
+        </>
 
-    </>     //fragment allows multiple arguments in return
-  )
+    );
 }
-
 export default App;
-
