42cf19e971c3bfcbe1ffdfdf652eeaf2efe58cf9
Yichen Wang <wyichen@bu.edu>
2022-09-23 22:20:56 -0400

    success in connect react to flask api

--- src/App.js
+++ src/App.js
@@ -31,6 +31,28 @@ function App() {
     const logOut = () => {
         setProfile(null);
     };
+    // api
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+    useEffect(() => {
+        // Using fetch to fetch the api from 
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    name: data.Name,
+                    age: data.Age,
+                    date: data.Date,
+                    programming: data.programming,
+                });
+            })
+        );
+    }, []);
 
     
     return (
@@ -74,6 +96,17 @@ function App() {
               <h1>Sentiment</h1>
             </div>
 
+            <div className="App">
+                <header className="App-header">
+                    <h1>React and flask</h1>
+                    {/* Calling a data from setdata for showing */}
+                    <p>{data.name}</p>
+                    <p>{data.age}</p>
+                    <p>{data.date}</p>
+                    <p>{data.programming}</p>
+
+                </header>
+            </div>
         </>
 
     );
